ea9aa286e1316bc84920a1eb5b7e4cfb4d682f1f
Johnathan Starr <john@msi-co.com>
2022-02-28 01:39:40 -0700

    Make scroll buttons consistent on window resize
    Set up custom hook to update window height on resize so the value in our scroll function is up to date with the user's activity.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,6 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
+
+{/*
+    Define custom hooks for window height so it will be updated on resize
+    We'll use it later.
+ */}
+function getWindowHeight() {
+    const height = window.innerHeight;
+    return height;
+}
+
+function useWindowHeight() {
+    const [ windowHeight, setWindowHeight ]= useState(getWindowHeight());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowHeight(getWindowHeight());
+        }
+
+        
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowHeight;
+}
 
 {/* 
     Set up styles for scroll button. Note the funky combo of positioning,
@@ -11,9 +36,9 @@ import styled from "styled-components";
 */}
 const ScrollButton = styled.button`
     position: sticky;
-    bottom: 2rem;
+    bottom: 1.5rem;
     margin-left: auto;
-    margin-right: 1rem;
+    margin-right: 1.5rem;
     flex-shrink: 0;
     height: 3.5rem;
     width: 3.5rem;
@@ -37,20 +62,8 @@ const ScrollButton = styled.button`
     }
 `
 
-function ScrollTopButton() {
-    function scrollTop() {
-        window.scroll({
-            top: 0,
-            left: 0, 
-            behavior: 'smooth',
-        });
-    }
-    
-      return <ScrollButton onClick={scrollTop}>{'\u25b2'}</ScrollButton>;
-}
-
 function ScrollBottomButton() {
-    const height = window.innerHeight;
+    const height = useWindowHeight();
 
     function scrollBottom() {
         window.scroll({
@@ -61,6 +74,18 @@ function ScrollBottomButton() {
     }
     
     return <ScrollButton onClick={scrollBottom}>{'\u25bc'}</ScrollButton>;
+}
+
+function ScrollTopButton() {
+    function scrollTop() {
+        window.scroll({
+            top: 0,
+            left: 0, 
+            behavior: 'smooth',
+        });
+    }
+    
+      return <ScrollButton onClick={scrollTop}>{'\u25b2'}</ScrollButton>;
 }
 
 function ScrollToTop() {
